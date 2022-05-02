bebae21012dde989189fd903681784eb07b7604f
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-05-02 19:05:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
 
 int main(){
-  return 0;
+   char c;
+    cout << "inserire un carattere";
+    cin >> c;
+    int n = (int) c;
+    bool x = funzione(n);
+
+    if (x==0) {
+        cout << "errore";
+    } else if ((65 <= n) && (n <= 90)) {
+           n=n+32;
+           cout <<(char)n;
+        }
+     else if ((97<=n) && (n<=122)){
+         n=n-32;
+         cout<<(char)n;
+    }
+    return 0;
 }
+ 

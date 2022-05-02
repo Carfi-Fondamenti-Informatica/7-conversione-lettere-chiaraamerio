0fe30dc55bfcf3317ace63cfbb2352160db726fd
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-05-02 19:29:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@ int main(){
     bool x = funzione(n);
 
    if (x==1){
-      if {((65 <= n)&&(n <= 90)){
+      if ((65 <= n)&&(n <= 90)){
          n=n+32;
          cout <<(char)n;
       } else if ((97 <=n)&&(n<=122)){
          n=n-32;
          cout<<(char)n;
-      } else {
+      }} else {
          cout<<"errore";
       }
           
